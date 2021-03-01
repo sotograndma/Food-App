7d5495310fb1a42cd463dcfc9fdf29db23bed85c
walkinglele <leleberjalan@gmail.com>
2021-03-01 09:57:53 +0700

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Dzaky Saputra on 01/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Dzaky Saputra on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
